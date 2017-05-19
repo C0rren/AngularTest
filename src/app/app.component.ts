1fcba4e5054a7f41150041de00a1a3ba8b0be342
Erik Dahlström <erik.e.dahlstrom@gmail.com>
2017-05-19 14:07:47 +0200

    learned to split up components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Hero } from './hero';
 @Component({
   selector: 'my-app',
   template: `
@@ -12,14 +12,7 @@ import { Component } from '@angular/core';
   			<span class="badge">{{hero.id}}</span>{{hero.name}}
 		</li>
 	</ul>
-	<div *ngIf="selectedHero">
-	  	<h2>{{ selectedHero.name }} details!</h2>
-	  	<div><label>id: </label>{{ selectedHero.id }}</div>
-	  	<div>
-	  		<label>name: </label>
-	  		<input [(ngModel)]="selectedHero.name" placeholder="name">
-	  	</div>
-  	</div>
+	<hero-detail [hero]="selectedHero"></hero-detail>
   	`,
   	styles: [`
 		.selected {
@@ -81,11 +74,6 @@ export class AppComponent  {
 	onSelect(hero: Hero): void {
   		this.selectedHero = hero;
 	}
-}
-
-export class Hero {
-	id: number;
-	name:string;
 }
 
 const HEROES: Hero[] = [
