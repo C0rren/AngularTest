e3336fd1ca0af787868e80a1ccc482533beed79e
Erik Dahlström <erik.e.dahlstrom@gmail.com>
2017-05-19 13:17:16 +0200

    learned ngModel

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,14 +9,25 @@ var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
         this.name = 'Angular';
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "\n  \t<h1>{{ title }}</h1>\n  \t<h2>{{ hero.name }} details!</h2>\n  \t<div><label>id: </label>{{ hero.id }}</div>\n  \t<div>\n  \t\t<label>name: </label>\n  \t\t<input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n  \t</div>\n  \t"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 //# sourceMappingURL=app.component.js.map
